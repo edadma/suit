63a5eac72356ae7dccabeea4f051358afde939f5
Edward A Maxedon, Sr <edadma@vinctus.com>
2021-03-29 20:32:12 -0400

    remove button animation temporarily

--- shared/src/main/scala/xyz/hyperreal/suit/Button.scala
+++ shared/src/main/scala/xyz/hyperreal/suit/Button.scala
@@ -16,53 +16,59 @@ class Button(text: String)(action: => Unit) extends Component {
 
   private val gs = font.getGlyphString(text)
 
-  protected val mouseEnterBackgroundColor: Int = Color.BLUE
+  protected val mouseEnterBorderColor: Int = Color.GREEN
   protected val mouseDownBackgroundColor: Int = Color.GREEN
 
-  val solidBorder = new SolidBorder(1, Color.BLACK)
+  val solidBorder = new SolidBorder(1, foregroundColor)
 
   padding = 5
   border = solidBorder
 
   listenTo(mouse)
-  listenTo(timer)
-
-  val granularity = 20
-
-  var t = 0.0
-  var hsl: HSL = null
+//  listenTo(timer)
+//
+//  val granularity = 20
+//
+//  var t = 0.0
+//  var hsl: HSL = null
 
   reactions += {
     case MouseEnter =>
-      t = 0
-      hsl = HSL(.26, 1, 0)
-      timer.start("enter", granularity)
-    case Tick("enter") =>
-      t += granularity / 1000.0
-
-      if (t >= .3)
-        timer.stop("enter")
-
-      val (r, g, b) = hsl.toRGB
-
-      solidBorder.color = r << 16 | g << 8 | b
-      hsl = hsl.luminosity(1 / (1 + exp(-(50 * t - 5))) * .5)
+      solidBorder.color = mouseEnterBorderColor
       repaint()
+
+//      t = 0
+//      hsl = HSL(.26, 1, 0)
+//      timer.start("enter", granularity)
+//    case Tick("enter") =>
+//      t += granularity / 1000.0
+//
+//      if (t >= .3)
+//        timer.stop("enter")
+//
+//      val (r, g, b) = hsl.toRGB
+//
+//      solidBorder.color = r << 16 | g << 8 | b
+//      hsl = hsl.luminosity(1 / (1 + exp(-(50 * t - 5))) * .5)
+//      repaint()
     case MouseExit =>
-      t = 0
-      hsl = HSL(.26, 1, 0.5)
-      timer.start("exit", granularity)
-    case Tick("exit") =>
-      t += granularity / 1000.0
-
-      if (t >= .3)
-        timer.stop("exit")
-
-      val (r, g, b) = hsl.toRGB
-
-      solidBorder.color = r << 16 | g << 8 | b
-      hsl = hsl.luminosity(1 / (1 + exp(-(50 * (.3 - t) - 5))) * .5)
+      solidBorder.color = foregroundColor
       repaint()
+
+//      t = 0
+//      hsl = HSL(.26, 1, 0.5)
+//      timer.start("exit", granularity)
+//    case Tick("exit") =>
+//      t += granularity / 1000.0
+//
+//      if (t >= .3)
+//        timer.stop("exit")
+//
+//      val (r, g, b) = hsl.toRGB
+//
+//      solidBorder.color = r << 16 | g << 8 | b
+//      hsl = hsl.luminosity(1 / (1 + exp(-(50 * (.3 - t) - 5))) * .5)
+//      repaint()
     case MouseClick(_, _) => action
   }
 
